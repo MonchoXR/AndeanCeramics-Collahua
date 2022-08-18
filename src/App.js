c500c0661d5c2facde592ad8aac5c98a96412c07
Martin Collahua <martin_adan2@hotmail.com>
2022-08-18 11:40:34 -0700

    Entrega Componente1

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import './App.css';
 // import Alerta from './components/Alerta/Alerta';
 import MiNavPublic from './components/Navbar/Navbar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import BannerPrincipal from './components/BannerPrincipal/BannerPrincipal';
 
 function App() {
   return (
     // <div className="App">
     <>
         <MiNavPublic></MiNavPublic>
+        <BannerPrincipal></BannerPrincipal>
         <ItemListContainer></ItemListContainer>
+
         
     </>
     // </div>
   );
 }
-
 export default App;
